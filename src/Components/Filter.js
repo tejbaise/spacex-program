9e0dc843ac4d664907d568744255b8f6ff2b08d0
tejbaise <tejbaise@gmail.com>
2020-09-20 21:21:35 +0530

    Added Filter Design, Corrected Page Layout

--- src/Components/Filter.js
+++ src/Components/Filter.js
@@ -1,9 +1,192 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../Stylesheets/Filter.scss';
-import Card from 'react-bootstrap/Card'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 
-function Filter() {
-  return <Card style={{ width: '14rem', backgroundColor: 'white' }}></Card>;
+let arr = [];
+for (let i = 2006; i <= 2019; i++) {
+  arr.push(i);
+}
+
+function Filter(props) {
+  const [filterState, setFilterState] = useState({
+    year: arr,
+    yearFilter: false,
+    successfulLaunch: true,
+    successfulLaunchFilter: false,
+    successfulLanding: true,
+    successfulLandingFilter: false,
+  });
+
+  useEffect(() => {
+    props.onFilterChange(filterState);
+  }, [filterState, props]);
+
+  let yearList = arr.map((item) => {
+    return (
+      <Col key={item}>
+        <Button
+          bsPrefix='FilterButton'
+          style={{
+            backgroundColor:
+              filterState.yearFilter && item === filterState.year[0]
+                ? '#70b102'
+                : '#bddb8f',
+          }}
+          value={item}
+          onClick={() => {
+            if (filterState.year.length === 1 && item !== filterState.year[0]) {
+              setFilterState({
+                ...filterState,
+                yearFilter: true,
+                year: [item],
+              });
+            }
+            if (filterState.year.length > 1) {
+              setFilterState({
+                ...filterState,
+                yearFilter: true,
+                year: [item],
+              });
+            }
+            if (filterState.year.length === 1 && item === filterState.year[0]) {
+              setFilterState({ ...filterState, yearFilter: false, year: arr });
+            }
+          }}
+          variant='primary'
+        >
+          {item}
+        </Button>
+      </Col>
+    );
+  });
+
+  return (
+    <Card style={{ width: '14rem', backgroundColor: 'white' }}>
+      <h2 style={{ fontWeight: 'bold' }}>Filters</h2>
+      <div className="CenterAlign">
+        <h6>Launch Year</h6>
+        <hr />
+          <Row xs={2} sm={2} md={2} lg={2} xl={2}>
+            {yearList}
+          </Row>
+        <h6>Successful Launch</h6>
+        <hr />
+          <Row>
+            <Col>
+              <Button
+                bsPrefix='FilterButton'
+                style={{
+                  backgroundColor:
+                    filterState.successfulLaunchFilter &&
+                    filterState.successfulLaunch
+                      ? '#70b102'
+                      : '#bddb8f',
+                }}
+                variant='primary'
+                disabled={
+                  filterState.successfulLaunchFilter &&
+                  !filterState.successfulLaunch
+                }
+                onClick={() => {
+                  setFilterState({
+                    ...filterState,
+                    successfulLaunchFilter: !filterState.successfulLaunchFilter,
+                    successfulLaunch: true,
+                  });
+                }}
+              >
+                True
+              </Button>
+            </Col>
+            <Col>
+              <Button
+                bsPrefix='FilterButton'
+                style={{
+                  backgroundColor:
+                    filterState.successfulLaunchFilter &&
+                    !filterState.successfulLaunch
+                      ? '#70b102'
+                      : '#bddb8f',
+                }}
+                variant='primary'
+                disabled={
+                  filterState.successfulLaunchFilter &&
+                  filterState.successfulLaunch
+                }
+                onClick={() => {
+                  setFilterState({
+                    ...filterState,
+                    successfulLaunchFilter: !filterState.successfulLaunchFilter,
+                    successfulLaunch: false,
+                  });
+                }}
+              >
+                False
+              </Button>
+            </Col>
+          </Row>
+        <h6>Successful Landing</h6>
+        <hr />
+          <Row>
+            <Col>
+              <Button
+                bsPrefix='FilterButton'
+                style={{
+                  backgroundColor:
+                    filterState.successfulLandingFilter &&
+                    filterState.successfulLanding
+                      ? '#70b102'
+                      : '#bddb8f',
+                }}
+                variant='primary'
+                disabled={
+                  filterState.successfulLandingFilter &&
+                  !filterState.successfulLanding
+                }
+                onClick={() => {
+                  setFilterState({
+                    ...filterState,
+                    successfulLandingFilter: !filterState.successfulLandingFilter,
+                    successfulLanding: true,
+                  });
+                }}
+              >
+                True
+              </Button>
+            </Col>
+            <Col>
+              <Button
+                bsPrefix='FilterButton'
+                style={{
+                  backgroundColor:
+                    filterState.successfulLandingFilter &&
+                    !filterState.successfulLanding
+                      ? '#70b102'
+                      : '#bddb8f',
+                }}
+                variant='primary'
+                disabled={
+                  filterState.successfulLandingFilter &&
+                  filterState.successfulLanding
+                }
+                onClick={() => {
+                  setFilterState({
+                    ...filterState,
+                    successfulLandingFilter: !filterState.successfulLandingFilter,
+                    successfulLanding: false,
+                  });
+                }}
+              >
+                False
+              </Button>
+            </Col>
+          </Row>
+          </div>
+    </Card>
+  );
 }
 
 export default Filter;
--- src/Components/ProgramDetails.js
+++ src/Components/ProgramDetails.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import '../Stylesheets/ProgramDetails.scss';
 import Container from 'react-bootstrap/Container'
 import Filter from './Filter';
 import List from './List';
 
 function ProgramDetails() {
+
+  const onFilterChange = useCallback((response) => {
+    //Add Filter Change Logic Here
+  }, []);
+
   return (
     <Container fluid='xl'>
       <h1>SpaceX Launch Programs</h1>
       <div className='FilterDisplay'>
-        <Filter />
+        <Filter onFilterChange={onFilterChange}/>
       </div>
       <div className='ListDisplay'>
         <List />
